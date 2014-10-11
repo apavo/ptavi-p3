3f0f24a33b0206f0dcf8e9e2c6683178f54af149
Alberto <alberto@ubuntu.(none)>
2014-10-11 12:53:00 +0200

    karaoke sin pep8

--- karaoke.py
+++ karaoke.py
@@ -1,24 +1,42 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 
-import SmallSMILHandler
+import smallsmilhandler
 import sys
 from xml.sax import make_parser
+import os
 
-def imprimir(sHandler):
-    for etiqueta in sHandler.lista:
-        print etiqueta[0], 
-        atributos=etiqueta[1]
-        for atributo in atributos:
-            print "\t", atributo, "=", atributos[atributo], 
-        print
+
+class KaraokeLocal(smallsmilhandler.SmallSMILHandler):
+
+    def __init__(self, fich):
+        parser = make_parser()
+        sHandler = smallsmilhandler.SmallSMILHandler()  
+        parser.setContentHandler(sHandler)
+        parser.parse(open(fich))
+        self.lista = sHandler.get_tags()    
+
+    def __str__(self):
+        for etiqueta in self.lista:
+            print etiqueta[0],
+            atributos =etiqueta[1]
+            for atributo in atributos:
+                print "\t", atributo, "=",atributos[atributo],
+            print
+
+    def do_local(self):
+        for etiqueta in self.lista:
+            atributos =etiqueta[1]
+            for atributo in atributos:
+                if atributo == "src":
+                    os.system("wget -nv " + atributos[atributo])
+                    nombre = atributos[atributo].split("/")
+                    print nombre[-1]
+
 try:
-    parser = make_parser()
-    sHandler = SmallSMILHandler.SmallSMILHandler()
-    parser.setContentHandler(sHandler)
-    parser.parse(open(sys.argv[1]))
-    imprimir(sHandler)
+    fich = sys.argv[1]
+    karaoke = KaraokeLocal(fich)
+    karaoke.__str__()
+    karaoke.do_local()
 except IndexError:
     print "Usage: python karaoke.py file.smil"
-
-    
